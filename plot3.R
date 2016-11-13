5947ecf576ee39da5fbfff9d1a92ba0d3e897fc7
Janne Flinck <TradingEmail90@yahoo.com>
2016-11-13 21:54:17 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -14,5 +14,4 @@ plot(a, sub1, type="l", ylab="Energy Submetering", xlab = "")
 lines(a, sub2, type="l", col="red")
 lines(a, sub3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
-dev.off()
 
