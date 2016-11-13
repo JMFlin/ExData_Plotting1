25d6cf40aa4d749db94d4ef48025b3bcbfbc0b75
Janne Flinck <TradingEmail90@yahoo.com>
2016-11-13 22:37:36 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -12,4 +12,4 @@ lines(a, as.numeric(my_subset$Sub_metering_2), type="l", col="red")
 lines(a, as.numeric(my_subset$Sub_metering_3), type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2,col=c("black", "red", "blue"), bty="o")
 plot(a, as.numeric(my_subset$Global_reactive_power), type="l", xlab="datetime", ylab="Global_reactive_power")
-
+dev.off()
