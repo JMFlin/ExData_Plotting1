73659f5239653b3e52bf60412f9cab4865bbe426
Janne Flinck <TradingEmail90@yahoo.com>
2016-11-13 22:33:25 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,6 @@
 my_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 my_subset <- my_data[my_data$Date %in% c("1/2/2007", "2/2/2007"), ]
 
-global <- as.numeric(my_subset$Global_active_power)
 png("plot1.png", width=480, height=480)
-hist(global, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(as.numeric(my_subset$Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
